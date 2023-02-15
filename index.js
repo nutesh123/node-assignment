cc49f506ba5d45b8697be53f634bab736be6bb05
harry <nuteshgorde@gmail.com>
2023-02-15 18:06:37 +0530

    task 7

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
 const http = require('http')
 
 const server = http.createServer((req , res )=>{
-    res.write('nutesh ')
-    res.end()
+    res.setHeader('Content-Type', 'text/html');
+    if(req.url == '/home'){
+        res.write('welcome to home ')
+        res.end()
+    }
+    if(req.url == '/about'){
+        res.write('welcome to about ')
+        res.end()
+    }
+    else(req.url == '/node')
+    {
+        res.write('welcome to node js')
+        res.end()
+    }
+    
+   
 })
 
 server.listen(3000)

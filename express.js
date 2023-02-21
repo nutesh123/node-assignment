7e9cef0b2a8b66880a43eed68696ad1de74bee89
harry <nuteshgorde@gmail.com>
2023-02-21 20:10:28 +0530

    task 4

--- express.js
+++ express.js
@@ -1,36 +1,18 @@
-const express = require('express')
+const express = require('express');
 const bodyParser = require('body-parser');
 
 const app = express();
 
-// Set up the body parser middlewa
-app.use(bodyParser.urlencoded({ extended: false }));
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
-app.get('/add-product',(req, res)=>{
-    res.send(`
-    <form method="POST" action="/add-product">
-    <label for="name">Product Name:</label>
-    <input type="text" id="name" name="name">
+app.use(bodyParser.urlencoded({extended: false}));
 
-    <label for="size">Product Size:</label>
-    <input type="text" id="size" name="size">
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
 
-    <button type="submit">Submit</button>
-  </form>
-  `)
-    
-})
+app.use((req, res, next) => {
+    res.status(404).send('<h1>Page not found</h1>');
+});
 
-app.post('/add-product', (req, res) => {
-    // Parse the form data using the body parser
-    const name = req.body.name;
-    const size = req.body.size
-  
-    // Log the parsed data to the console
-    console.log(size);
-  
-    // Send a response back to the client
-    res.send(`Product added: ${name}, ${size}`);
-  });
-
-app.listen(3000)
+app.listen(3000);
